46aa58b18fa4f7d66fa450f605486136d1044d20
Natalie S. <161007805+natalie-christine@users.noreply.github.com>
2024-08-29 19:11:44 +0200

    Add files via upload

---
+++ crawler/ArticleFetcher.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import time
+from .CrawledArticle import CrawledArticle
+
+class ArticleFetcher():
+    def fetch(self):
+        url = "http://python.beispiel.programmierenlernen.io/index.php"
+      #  articles = []
+                 
+        while url != "":  
+            print(url)  
+            time.sleep(1)
+            r = requests.get(url)
+            doc = BeautifulSoup(r.text, "html.parser")  #"lxml"
+            
+            for card in doc.select(".card"):
+                emoji = card.select_one(".emoji").text
+                content = card.select_one(".card-text").text
+                title  = card.select(".card-title span")[1].text
+                image = urljoin(url, card.select_one("img").attrs["src"])
+        
+                yield CrawledArticle(title,emoji,content, image) #crawled
+                #articles.append(crawled)
+                
+            next_button = doc.select_one(".navigation .btn")
+            if next_button:
+                next_href = next_button.attrs["href"]
+                next_href = urljoin(url, next_href)
+                url = next_href
+            else:
+                url = ""
+            
+        #return articles
