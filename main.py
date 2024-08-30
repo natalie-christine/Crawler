c8b48d4441d864cca14188cf6383644a83253d5f
Natalie S. <161007805+natalie-christine@users.noreply.github.com>
2024-08-30 10:42:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,15 @@ fetcher = crawler.ArticleFetcher()
     # print("Image URL:", article.image)
     # print("---")
     
-
 with open ('crawler_output', 'w', newline= '', encoding='utf-8') as csvfile: 
-    articlewriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    # spamwriter.writerow(['Spam']*5 + ['Baked beans'])
-    # spamwriter.writerow(['Spam, Lovely Spam', 'Wonderful Spam'])
-    
+    articlewriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)  
+      
+    counter = 0
     for article in fetcher.fetch():
+        if counter == 10:
+            break
+        counter = counter +1
+        print(article.emoji + ": " + article.title)
         articlewriter.writerow([article.emoji, article.title,article.content,article.image])
-        
-
-counter = 0
-for article in fetcher.fetch():
-    if counter == 16:
-        break
-    counter = counter +1
-    print(article.emoji + ": " + article.title)
     
+   
